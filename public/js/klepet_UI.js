c736e56c1898562ac2d9b1b350351972223acc74
Andraz Povse <andraz.povse1@gmail.com>
2016-04-07 12:27:03 +0000

    youtube video probam postavit na konec

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -141,6 +141,17 @@ function dodajSmeske(vhodnoBesedilo) {
 
 function dodajVideo(vhodnoBesedilo){
 
+  if (vhodnoBesedilo.match(/((http(s)?:\/\/)?)(www\.)?((youtube\.com\/)|(youtu.be\/))[\S]+/gi)){
+    var linkiVbesedilu = vhodnoBesedilo.match(/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/gi);
+    //pride slika za celotnim stringom
+    for (var i = 0; i < linkiVbesedilu.length; i++){  //vrinemo slike na konec besedila
+     vhodnoBesedilo = vhodnoBesedilo + "<iframe width='200px' height='150px' style='margin-left:20px;' src='https://www.youtube.com/embed/" + linkiVbesedilu[i]+ "' allowfullscreen ></iframe>");
+    }
+  }
+  
+  return vhodnoBesedilo;
+  
+  /* NAMESTO HTMLJA je bil dodan video
   var kodaVidea = youtubeVideoId(vhodnoBesedilo);  //rabimo ID youtube posnetka, nato zamenjamo youtube url z HTMLjem 
   vhodnoBesedilo = vhodnoBesedilo.replace(/((http(s)?:\/\/)?)(www\.)?((youtube\.com\/)|(youtu.be\/))[\S]+/gi,"<iframe width='200px' height='150px' style='margin-left:20px;' src='https://www.youtube.com/embed/"+kodaVidea+"' allowfullscreen ></iframe>" );
   return vhodnoBesedilo;
@@ -149,4 +160,4 @@ function dodajVideo(vhodnoBesedilo){
 function youtubeVideoId(besedilo) { //dobimo ID video posnetka iz youtube......vidi link, in za v= odseka 11 znakov ki so ID videa
   var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   return (besedilo.match(p)) ? RegExp.$1 : false;
-}
+}*/
